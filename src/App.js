0fe31575bbbbb7316728688b4cb861dc01ea0c2b
ramansrsta <ramansresta@gmail.com>
2020-12-16 13:21:07 +0545

    [added state and default required validation]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [ formData, setFormData] = useState({
+      email : '',
+      password: ''
+    });
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      setFormData({
+        email: '',
+        password: ''
+      })
+    }
+
+    const onChangeHandler = event => {
+      setFormData({
+        [event.target.name] : event.target.value
+      })
+    }
+
+    const useStyles = makeStyles(() => ({
+      root: {
+          marginTop: '3%',
+          marginLeft: '20%',
+          padding: '12px',
+          width: '60%',
+          display: 'flex',
+          flexDirection: 'column'
+      },
+      button: {
+        marginTop: '3%'
+      },
+      header: {
+        textAlign:'center'
+      }
+    }));
+
+    const classes = useStyles();
+
+    return (
+      <div className={ App }>
+        <h1 className={classes.header}> Login Page </h1>
+        <form className={classes.root} autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            required
+            id="standard-email-input"
+            label="Email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={onChangeHandler}
+          />
+
+          <TextField
+            required
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={onChangeHandler}
+          />
+          
+          <Button type="submit" className={classes.button} variant="contained" color="primary">
+             Submit
+          </Button>
+        </form>
+      </div>
+    
+    );
+  }
 
 export default App;
